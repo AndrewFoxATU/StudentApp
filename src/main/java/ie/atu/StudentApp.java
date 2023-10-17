7358602e55ddcf47826d257e81bf79c50102f5bb
AndrewFoxATU <G00417373@atu.ie>
2023-10-17 14:33:18 +0100

    added Students info to studentApp.java

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -1,5 +1,12 @@
 package ie.atu;
 
+import java.util.Scanner;
 public class StudentApp {
+    String studentName;
+    String studentEmail;
+    String studentCourse;
 
+    public void displayInfo() {
+        System.out.println("Name: " + studentName + " - " + studentEmail + ", Course: " + studentCourse);
+    }
 }
