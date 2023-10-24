0c43592935cf0840f0dc83bd0999454bdae3934e
AndrewFoxATU <G00417373@atu.ie>
2023-10-24 13:12:18 +0100

    added Constructors to studentApp.java

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -6,6 +6,21 @@ public class StudentApp {
     String studentEmail;
     String studentCourse;
 
+
+    // Default Constructor
+    public  StudentApp() {
+        this.studentName = "John";
+        this.studentEmail = "JohnDoe@atu.ie";
+        this.studentCourse = "Software";
+    }
+
+    // Constructor
+    public StudentApp(String studentName, String studentEmail, String studentCourse){
+        this.studentName = studentName;
+        this.studentEmail = studentEmail;
+        this.studentCourse = studentCourse;
+    }
+
     public void displayInfo() {
         System.out.println("Name: " + studentName + " - " + studentEmail + ", Course: " + studentCourse);
     }
